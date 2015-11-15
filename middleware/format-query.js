25538a1a7e9c866cdc93de79c871bfcec533ec8f
Linus Miller <lohfu@lohfu.io>
2015-11-16 00:32:01 +0100

    Allow '=' delimited query values, so one can use internal mongodb operators like .

--- middleware/format-query.js
+++ middleware/format-query.js
@@ -1,10 +1,20 @@
+var evals = [ 'undefined', 'null', 'false', 'true' ];
+
 module.exports = function(req, res, next) {
 	req.query = _.mapValues(req.query, function(value, key) {
 		value = decodeURIComponent(value);
 
+		var arr;
+
 		if(/^\//.test(value)) {
-			var arr = value.match(/\/(.*)\/(.*)/);
+			arr = value.match(/\/(.*)\/(.*)/);
 			value = new RegExp(arr[1], arr[2] || undefined);
+		} else if(/=/.test(value)) {
+			arr = value.split('=');
+
+			value = {};
+
+			value[arr[0]] = evals.indexOf(arr[1]) > -1 ? eval(arr[1]) : arr[1];
 		}
 
 		return value;
