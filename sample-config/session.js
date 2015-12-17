4099c1d75a12baca471e08a68e319a2606022e7e
Linus Miller <lohfu@lohfu.io>
2015-12-17 16:39:04 +0100

    Fixed redis (session) config.

--- sample-config/session.js
+++ sample-config/session.js
@@ -1,15 +1,31 @@
-var defaults = {
-	secret: 'changeThisFool',
+var redisStore;
+
+var config = {
+	secret: 'change-this-fool',
 	resave: true,
 	saveUninitialized: true
 };
 
-module.exports = _.merge(defaults, {
-	production: {
-		redis: {
-			port: 6379,
-			host: 'localhost'
-		}
-	}
-}[ENV]);
+var redisConfig = {
+	host: 'localhost',
+	port: 6379
+};
 
+if(ENV === 'production') {
+	var RedisStore = require('connect-redis')(require('express-session'));
+
+	redisStore = new RedisStore(this.redis);
+
+	redisStore.on('connect', function(){
+		console.info("Redis connected succcessfully");
+	});
+
+	redisStore.on('disconnect', function(){
+		console.error("[" + chalk.red('ERROR') + "]  Unable to connect to redis. Has it been started?"); 
+		process.exit(2);
+	});
+
+	config.store = redisStore;
+}
+
+module.exports = config;
