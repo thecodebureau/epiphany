a293b666d02b4bd79c72582f58a018b25e9b8580
Linus Miller <lohfu@lohfu.io>
2015-12-27 20:07:35 +0100

    Refactored dust.dependencies method.

--- dust-extensions.js
+++ dust-extensions.js
@@ -1,5 +1,4 @@
 var dust = require('dustjs-linkedin');
-var _ = require('lodash');
 
 dust.compiled = function(template) {
 	return this.template(template).compiled;
@@ -9,29 +8,25 @@ dust.src = function(template) {
 	return this.template(template).src;
 };
 
-dust.dependencies = function(template) {
-	var all = [],
-		check = [ dust.template(template).dependencies ],
-		deps,
-		prev = null;
+function dependencies(template) {
+	return dust.template(template).dependencies;
+}
 
-	while(deps = check.shift()) {
-		// allow circular reference in templates, mainly because of navigation-item.
-		// missing templates will be loaded asyncronously anyway
-		if(deps === prev) continue;//throw new Error('Circular reference in Dust templates.');
+dust.dependencies = function(templates) {
+	if(!_.isArray(templates)) templates = [ templates ];
 
-		all = _.union(all, deps);
+	var collected = [],
+		check = templates;
 
-		for(var i = 0; i < deps.length; i++) {
-			var dependencies = dust.template(deps[i]).dependencies;
+	while(check.length) {
+		var deps = _.unique(_.compact(_.flatten(_.map(check, dependencies))));
 
-			if(dependencies) check.push(dependencies);
-		}
+		check = _.difference(deps, collected, templates);
 
-		prev = deps;
+		collected = collected.concat(check);
 	}
 
-	return all;
+	return collected;
 };
 
 dust.template = function(templateName) {
