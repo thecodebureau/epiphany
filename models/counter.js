1fafc5d7277dd6d23bb98b23c699b35ad1ef63f2
Linus Miller <lohfu@lohfu.io>
2016-03-07 19:37:49 +0100

    Updated counter model to get with the program.

--- models/counter.js
+++ models/counter.js
@@ -1,16 +1,15 @@
+var mongoose = require('mongoose');
 // counters used for auto-incrementing sequence fields
 
-module.exports = function(mongoose, schemas, plugins) {
-	var CounterSchema = new mongoose.Schema({
-		_id: String,
-		seq: Number
+var CounterSchema = new mongoose.Schema({
+	_id: String,
+	seq: Number
+});
+
+CounterSchema.statics.getNextSequence = function(name, callback) {
+	this.findOneAndUpdate({ _id: name }, { $inc: { seq: 1 } }, function(err, counter) {
+		callback(err, counter && counter.seq);
 	});
-
-	CounterSchema.statics.getNextSequence = function(name, callback) {
-		this.findOneAndUpdate({ _id: name }, { $inc: { seq: 1 } }, function(err, counter) {
-			callback(err, counter && counter.seq);
-		});
-	};
-
-	mongoose.model('Counter', CounterSchema);
 };
+
+module.exports = mongoose.model('Counter', CounterSchema);
